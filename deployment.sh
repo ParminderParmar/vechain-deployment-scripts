d69fdb542fa0a7ab6a86d66980f28d2d2ede2134
Parminder Parmar <parmarparminder@gmail.com>
2020-03-25 21:26:21 +0000

    Reducing log level

--- deployment.sh
+++ deployment.sh
@@ -21,7 +21,7 @@ echo "---"
 # Installing pre-requisites for VeChain deployment   #
 #################################################################
 echo "Installing prerequisites..."
-sudo apt-get -y install build-essential libgflags-dev libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev git
+sudo apt-get -y install build-essential libgflags-dev libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev git > /dev/null 2>&1
 echo "---"
 #################################################################
 # Downloading and installing Go dependency #
@@ -30,10 +30,10 @@ echo "Installing go..."
 cd ~
 export GOPATH=$HOME/go
 mkdir -p $GOPATH/src
-wget https://dl.google.com/go/go1.14.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.14.linux-amd64.tar.gz
-chmod +x /usr/local/go/bin/go
-rm go1.14.linux-amd64.tar.gz
+wget https://dl.google.com/go/go1.14.linux-amd64.tar.gz > /dev/null 2>&1
+tar -C /usr/local -xzf go1.14.linux-amd64.tar.gz > /dev/null 2>&1
+chmod +x /usr/local/go/bin/go > /dev/null 2>&1
+rm go1.14.linux-amd64.tar.gz > /dev/null 2>&1
 echo "---"
 #################################################################
 # Downloading and installing Dep dependency   #
@@ -41,16 +41,16 @@ echo "---"
 echo "Installing dep..."
 export PATH=$PATH:/usr/local/go/bin
 cd /usr/local/bin/
-wget https://github.com/golang/dep/releases/download/v0.5.4/dep-linux-amd64
-ln -s dep-linux-amd64 dep
-chmod +x /usr/local/bin/*
+wget https://github.com/golang/dep/releases/download/v0.5.4/dep-linux-amd64 > /dev/null 2>&1
+ln -s dep-linux-amd64 dep > /dev/null 2>&1
+chmod +x /usr/local/bin/* > /dev/null 2>&1
 echo "---"
 #################################################################
 # Cloning VeChain git repositor and build the complete suite  #
 #################################################################
 echo "Installing and configuring VeChain..."
-git clone https://github.com/vechain/thor.git $GOPATH/src/VeChain/thor
+git clone https://github.com/vechain/thor.git $GOPATH/src/VeChain/thor > /dev/null 2>&1
 cd $GOPATH/src/VeChain/thor
-make dep
-make all
+make dep > /dev/null 2>&1
+make all > /dev/null 2>&1
 echo "---"
