83310e6b91a566e64d2c841597faf3b14b4fd628
Parminder Parmar <parmarparminder@gmail.com>
2020-03-25 21:20:53 +0000

    Adding script documentation

--- deployment.sh
+++ deployment.sh
@@ -4,16 +4,28 @@ clear
 
 network=$1
 
+#################################################################
+# VeChain Deployment script #
+#################################################################
 echo "This script will setup a VeChain node."
 echo "---"
+#################################################################
+# Updating and upgrading linux environment   #
+#################################################################
 echo "Performing a general system update (this might take a while)..."
 sudo apt-get update > /dev/null 2>&1
 sudo apt-get -y upgrade > /dev/null 2>&1
 sudo apt-get -y dist-upgrade > /dev/null 2>&1
 echo "---"
+#################################################################
+# Installing pre-requisites for VeChain deployment   #
+#################################################################
 echo "Installing prerequisites..."
 sudo apt-get -y install build-essential libgflags-dev libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev git
 echo "---"
+#################################################################
+# Downloading and installing Go dependency #
+#################################################################
 echo "Installing go..."
 cd ~
 export GOPATH=$HOME/go
@@ -23,6 +35,9 @@ tar -C /usr/local -xzf go1.14.linux-amd64.tar.gz
 chmod +x /usr/local/go/bin/go
 rm go1.14.linux-amd64.tar.gz
 echo "---"
+#################################################################
+# Downloading and installing Dep dependency   #
+#################################################################
 echo "Installing dep..."
 export PATH=$PATH:/usr/local/go/bin
 cd /usr/local/bin/
@@ -30,6 +45,9 @@ wget https://github.com/golang/dep/releases/download/v0.5.4/dep-linux-amd64
 ln -s dep-linux-amd64 dep
 chmod +x /usr/local/bin/*
 echo "---"
+#################################################################
+# Cloning VeChain git repositor and build the complete suite  #
+#################################################################
 echo "Installing and configuring VeChain..."
 git clone https://github.com/vechain/thor.git $GOPATH/src/VeChain/thor
 cd $GOPATH/src/VeChain/thor
